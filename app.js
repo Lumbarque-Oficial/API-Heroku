e29ab1a0b9188ae7d81dcdc9c4902b0305c41749
lumb <67676290+Lumbarque-Oficial@users.noreply.github.com>
2020-10-23 23:15:51 -0300

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const PORT = process.env.PORT || 8877
+
+app.use(cors())
 
 app.get('/about', (req, res) => {
 	res.json({
